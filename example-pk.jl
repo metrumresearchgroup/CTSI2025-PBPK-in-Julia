7eef8644779ac0d7f2a8b7f509396314de3ae36c
ahmede <ahmede@metrumrg.com>
2025-01-09 02:06:55 -0500

    check pk fig

--- example-pk.jl
+++ example-pk.jl
@@ -7,11 +7,70 @@ using InteractiveUtils
 # ╔═╡ 76902963-43fc-405d-9132-2ed8409fa56d
 using PlutoUI
 
-# ╔═╡ 391eef46-ce42-11ef-1404-234348e7f732
+# ╔═╡ 01d1f036-99f2-4ef6-8ab3-f1e5180fd5c5
 using DifferentialEquations, ModelingToolkit, Unitful
 
 # ╔═╡ 179bbe7a-16f1-466f-baa0-a132a4f916ce
 TableOfContents()
+
+# ╔═╡ c3f2054a-8df4-41ea-9062-e08fb4b1792b
+md"
+# PK example
+"
+
+# ╔═╡ a0e14355-0fbd-4b98-8790-e7ec50910a61
+md"
+Let's build a one-compartment PK model with oral absorption
+"
+
+# ╔═╡ 83701d28-60a8-4c11-8923-ef7be27d384d
+LocalResource(
+	"figs/pk.png",
+	:width => 400
+)
+
+# ╔═╡ 3a1c3888-1671-4bcc-bd0f-884d6e0e9365
+md"
+## Load libraries
+"
+
+# ╔═╡ c4a4b27e-d053-4f70-aa2a-5ab7538780ee
+md"
+## Build model
+"
+
+# ╔═╡ ac7b62aa-29ec-4b61-a01e-8a582b10e928
+function PK(; name)
+    
+    @independent_variables t [description = "time"]  #unit = u"hr"]
+    Dt = Differential(t)
+    
+    pars = @parameters begin
+        CL = 4.0, [description = "clearance"]  #unit = u"L*hr^-1"]
+        V = 35.0, [description = "volume"]  #unit = u"L"]
+        ka = 2.0, [description = "absorption rate constant"]  #unit = u"hr^-1"]
+    end
+
+    vars = @variables begin
+		(depot(t) = 320.0), [description = "Theophylline amount in depot compartment"]  #unit = u"mg"]
+        (cent(t) = 0.0), [description = "Theophylline amount in central compartment"]  #unit = u"mg"]
+		c_cent(t), [description = "Theophylline concentration in central compartment"]  #unit = u"mg*L^-1"]
+	end
+	
+    observed = [
+        c_cent ~ cent/V
+    ]
+
+	eqs = [
+		Dt(depot) ~ -ka * depot,
+		Dt(cent) ~ ka * depot - (CL / V) * cent
+	]
+
+	ODESystem(eqs, t, vars, pars; name=name, observed=observed, checks = ~ModelingToolkit.CheckUnits)  # 100124 unit checking is broken at this point so will suppress for now
+end
+
+# ╔═╡ 3604e6c4-a29d-45ee-af90-ae6d480a185b
+@mtkbuild pk = PK()
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -2193,6 +2252,13 @@ version = "17.4.0+2"
 # ╔═╡ Cell order:
 # ╟─76902963-43fc-405d-9132-2ed8409fa56d
 # ╟─179bbe7a-16f1-466f-baa0-a132a4f916ce
-# ╠═391eef46-ce42-11ef-1404-234348e7f732
+# ╟─c3f2054a-8df4-41ea-9062-e08fb4b1792b
+# ╟─a0e14355-0fbd-4b98-8790-e7ec50910a61
+# ╟─83701d28-60a8-4c11-8923-ef7be27d384d
+# ╟─3a1c3888-1671-4bcc-bd0f-884d6e0e9365
+# ╠═01d1f036-99f2-4ef6-8ab3-f1e5180fd5c5
+# ╟─c4a4b27e-d053-4f70-aa2a-5ab7538780ee
+# ╠═ac7b62aa-29ec-4b61-a01e-8a582b10e928
+# ╠═3604e6c4-a29d-45ee-af90-ae6d480a185b
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
