1330a3815fd504429cf5dc340a04fbc91366a4cb
ahmede <ahmede@metrumrg.com>
2025-01-09 12:40:38 -0500

    update example pk

--- example-pk.jl
+++ example-pk.jl
@@ -113,16 +113,54 @@ md"
 # ╔═╡ 445b8741-6716-4684-8c07-2e171f070abb
 sol = solve(prob, Tsit5())
 
-# ╔═╡ afe9cc47-1122-4376-944d-377bbd5fa0a4
-md"
-## Plot
-"
+# ╔═╡ d2582ca6-5ff3-49d4-8241-9703f976dd85
+md"### Plot"
 
-# ╔═╡ 8166670f-bd07-4c79-99ef-ebe899177365
+# ╔═╡ 73f7a96d-5f66-4ab4-82bb-0627a6139f8a
 plot(sol)
 
-# ╔═╡ 50a44f05-10ae-4f89-b6cc-68358efa126f
-plot(sol, idxs = :cent)
+# ╔═╡ 841eaf44-a093-4cc9-a792-184d11622dac
+plot(sol, idxs = :c_cent)
+
+# ╔═╡ 1ae0468a-0684-405d-addb-a918030df470
+md"### Callbacks"
+
+# ╔═╡ 12777c22-e6b2-47fb-aff6-3a533faa1680
+idx_depot = ModelingToolkit.variable_index(pk, :depot)
+
+# ╔═╡ 27799b5f-c1a4-4539-ae05-5fa3f43ce122
+begin
+	affect!(integrator) = integrator.u[idx_depot] += 320.0
+	cb = PresetTimeCallback([24.0, 48.0], affect!)
+end
+
+# ╔═╡ 7f8f500f-3784-4b10-89a3-6212a18ead24
+begin
+	prob_cb = remake(prob, tspan=(0.0,72.0))
+	sol_cb = solve(prob_cb, callback=cb)
+end
+
+
+# ╔═╡ fcc3498d-aae0-4ffd-8b09-b1ba19935be9
+plot(sol_cb)
+
+# ╔═╡ c26b08ef-bbcf-47c8-8a7f-3e2a23bd9c06
+md"### Update parameter"
+
+# ╔═╡ b2f61109-3de3-4b8e-9ab7-457d32afd598
+prob.ps[:ka]
+
+# ╔═╡ 62da16d4-f06c-4f89-88b3-5fc80e07f0fc
+prob_up = remake(prob; p = [:ka => 0.5])
+
+# ╔═╡ 96df73e4-19f7-4a4a-b70e-aac0554a6252
+prob_up.ps[:ka]
+
+# ╔═╡ 77cb307b-4286-4e5d-b4a4-c1f421922682
+begin
+	sol_up = solve(prob_up, Tsit5())
+	plot(sol_up, idxs = :c_cent)
+end
 
 # ╔═╡ Cell order:
 # ╟─76902963-43fc-405d-9132-2ed8409fa56d
@@ -146,6 +184,16 @@ plot(sol, idxs = :cent)
 # ╠═1a062802-ee3b-493c-b260-9a8c784a1f21
 # ╟─c49719ee-7f27-4eaf-aeb4-ee551e91427c
 # ╠═445b8741-6716-4684-8c07-2e171f070abb
-# ╟─afe9cc47-1122-4376-944d-377bbd5fa0a4
-# ╠═8166670f-bd07-4c79-99ef-ebe899177365
-# ╠═50a44f05-10ae-4f89-b6cc-68358efa126f
+# ╟─d2582ca6-5ff3-49d4-8241-9703f976dd85
+# ╠═73f7a96d-5f66-4ab4-82bb-0627a6139f8a
+# ╠═841eaf44-a093-4cc9-a792-184d11622dac
+# ╟─1ae0468a-0684-405d-addb-a918030df470
+# ╠═12777c22-e6b2-47fb-aff6-3a533faa1680
+# ╠═27799b5f-c1a4-4539-ae05-5fa3f43ce122
+# ╠═7f8f500f-3784-4b10-89a3-6212a18ead24
+# ╠═fcc3498d-aae0-4ffd-8b09-b1ba19935be9
+# ╟─c26b08ef-bbcf-47c8-8a7f-3e2a23bd9c06
+# ╠═b2f61109-3de3-4b8e-9ab7-457d32afd598
+# ╠═62da16d4-f06c-4f89-88b3-5fc80e07f0fc
+# ╠═96df73e4-19f7-4a4a-b70e-aac0554a6252
+# ╠═77cb307b-4286-4e5d-b4a4-c1f421922682
