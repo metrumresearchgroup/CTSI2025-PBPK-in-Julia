837e77531738e748d448815e417330ef6afbdb25
ahmede <ahmede@metrumrg.com>
2025-01-09 01:01:03 -0500

    check basics notebook

---
+++ julia-basics.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.20.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ dd5df476-ce4e-11ef-2e20-a90209692b8c
+
+
+# ╔═╡ Cell order:
+# ╠═dd5df476-ce4e-11ef-2e20-a90209692b8c
